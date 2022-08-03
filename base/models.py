7ed5b51197710a951c5f5ecacb82bfae48ae5e2f
Avishka Kavinda <dfntlynotace@gmail.com>
2022-08-03 21:46:51 +0530

    Created the project db model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    languages = (
+        ('Python', 'Python'),
+        ('Js', 'Javascript'),
+        ('C#', 'C#'),
+        ('Java', 'Java'),
+    )
+    
+    Name = models.CharField(max_length=250)
+    firstCommit= models.DateField()
+    lastCommit= models.DateField()
+    mainStack = models.CharField(max_length=6, choices=languages)
+    description = models.CharField(max_length=500)
+    details = models.TextField(default="None")
+    bannerImg = models.URLField()
+    github = models.URLField()
+     
+    def __str__(self):
+        return self.name
